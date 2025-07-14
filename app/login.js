5fbc2f33914e0989a3d9b04ba6c805b9acc16db1
Susilvav03 <susilvav@unal.edu.co>
2025-07-14 15:44:19 -0500

    Modify: functionality for login done and added  the session persistence

---
+++ app/login.js
@@ -0,0 +1,43 @@
+
+function render() {}
+
+function afterRender() {
+  const loginForm = document.getElementById('login-form');
+
+  if (loginForm) {
+    loginForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+
+      const formData = new FormData(loginForm);
+      
+      
+      let data = {};
+      for (const [key, value] of formData.entries()) {
+       data[key] = value;
+      }
+      console.log(formData.entries());
+      if (!data.email || !data.password) {
+        alert("Hay campos vacíos");
+        return;
+      }
+
+      const emailFetch = await fetch(`http://localhost:3000/users?email=${data.email}`);
+      const emailResponse = await emailFetch.json();
+
+      if (emailResponse[0]) {
+        if (emailResponse[0].password === data.password) {
+          alert("Inicio de sesión exitoso.");
+          sessionStorage.setItem('logged', "true");
+          sessionStorage.setItem('session', JSON.stringify(emailResponse[0]));
+          window.location.href = '#/dashboard';
+        } else {
+          alert("Correo, usuario o contraseña incorrectos");
+        }
+      } else {
+        alert("Correo, usuario o contraseña incorrectos");
+      }
+    });
+  }
+}
+
+afterRender()
